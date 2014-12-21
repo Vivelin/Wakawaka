5bc0a876dd3b15e610364a2dded6a87fa54bf3cf
Ruben Verdoes <horsedrowner@gmail.com>
2014-12-21 19:16:58 +0100

    Write rendered documentation to files
    Closes #5.

--- Wakawaka.App/Program.cs
+++ Wakawaka.App/Program.cs
@@ -6,6 +6,8 @@ namespace Wakawaka.App
 {
     class Program
     {
+        private static string outputFolder;
+
         static void Main(string[] args)
         {
             Console.Title = "Wakawaka";
@@ -13,24 +15,20 @@ namespace Wakawaka.App
             if (args.Length > 0)
             {
                 var fileName = args[0];
+                outputFolder = args?[1] ?? Environment.CurrentDirectory;
+
+                if (!Directory.Exists(outputFolder))
+                    Directory.CreateDirectory(outputFolder);
 
                 var xmlDoc = XmlDocumentation.Load(fileName);
-
                 var types = from member in xmlDoc.GetMembers()
-                            where member.ID.Prefix == ID.MemberType.Method
+                            where member is Documentation.Type
                             orderby member.ID.FullName ascending
                             select member;
 
                 foreach (var member in types)
                 {
-                    using (var baseWriter = new StringWriter())
-                    {
-                        var writer = new MarkdownTextWriter(baseWriter);
-                        member.Render(writer);
-
-                        Console.WriteLine(baseWriter.ToString());
-                        Console.WriteLine();
-                    }
+                    RenderMember(member);
                 }
             }
             else
@@ -38,6 +36,29 @@ namespace Wakawaka.App
                 Console.Error.WriteLine("Usage: {0} filename",
                     AppDomain.CurrentDomain.FriendlyName);
             }
+        }
+
+        private static void RenderMember(Documentation.Member member)
+        {
+            var path = GenerateFileName(member);
+            var stream = new FileStream(path, FileMode.OpenOrCreate);
+            var writer = new StreamWriter(stream);
+
+            try
+            {
+                var markdownWriter = new MarkdownTextWriter(writer);
+                member.Render(markdownWriter);
+            }
+            finally
+            {
+                writer.Close();
+            }
+        }
+
+        private static string GenerateFileName(Documentation.Member member)
+        {
+            var name = member.ID.FullName + ".md";
+            return Path.Combine(outputFolder, name);
         }
     }
 }
